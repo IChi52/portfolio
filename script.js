30e7a587430cc32fd17c962c088d54abcde89643
IChi52 <145050378+IChi52@users.noreply.github.com>
2024-03-06 14:16:05 +0800

    update index content

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const tabContents = document.querySelectorAll('[data-tab-content]');
 
 tabs.forEach(tab => {
     tab.addEventListener('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
         const targetId = tab.getAttribute('href').substring(1);
         const targetContent  = document.getElementById(targetId);
-
 
         tabs.forEach(t => t.classList.remove('active'));
         tabContents.forEach(tabContent => {
@@ -15,5 +16,31 @@ tabs.forEach(tab => {
 
         tab.classList.add("active");
         targetContent.classList.add('active');
+        console.log("tab selected: ", tab.href)
+
     })
 })
+
+
+// slideshow, taken from https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 1;
+showSlides(slideIndex);
+function showSlides(n){
+    const slides = document.getElementsByClassName("slide-item");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for(let i = 0; i<slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+function adjacentSlide(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
